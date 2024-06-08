eb9f1b815d42651aab0e4e378f7bff5a784aaa9e
suji5262 <suji5262@gmail.com>
2024-06-08 23:53:45 +0900

    :sparkles: Bank file add

---
+++ src/main/java/finant/mini/domain/bank/entity/Bank.java
@@ -0,0 +1,37 @@
+package finant.mini.domain.bank.entity;
+
+import finant.mini.domain.product.entity.LoanProduct;
+import finant.mini.domain.product.entity.Product;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Builder
+@Slf4j
+@Table(name = "bank")
+public class Bank {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "BANK_ID")
+    private Long id;
+
+    @Column(name = "BANK_NAME", nullable = false) // 은행명
+    private String name;
+
+    @Column(name = "ICON_PATH", columnDefinition = "text") // 은행로고
+    private String imgPath;
+
+    @OneToMany(mappedBy = "bank") // 상품
+    private List<Product> products = new ArrayList<>();
+
+    @OneToMany(mappedBy = "bank") // 관심상품
+    private List<LoanProduct> loanProducts = new ArrayList<>();
+}
